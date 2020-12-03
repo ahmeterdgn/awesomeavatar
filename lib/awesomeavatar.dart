2bb9994c57a35b77cde03aea8f2f45a00c009229
Ahmet Erdoğan <48730205+ahmeterdgn@users.noreply.github.com>
2020-12-03 14:50:56 +0100

    Update

--- lib/awesomeavatar.dart
+++ lib/awesomeavatar.dart
@@ -1,6 +1,6 @@
 
 import 'dart:async';
-
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class Awesomeavatar {
@@ -10,5 +10,38 @@ class Awesomeavatar {
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+  double radius;
+  String networkImage;
+  String assetsImage;
+  double outRadius;
+  Color backgroundColor;
+  Awesomeavatar({
+    this.radius,
+    this.networkImage,
+    this.assetsImage,
+    this.outRadius,
+    this.backgroundColor,
+  });
+
+  Widget build(BuildContext context) {
+  var image = networkImage !=null ? Image.network(
+      networkImage,
+    ) :assetsImage !=null ? Image.asset(
+    assetsImage,
+  ) : Container();
+
+    return CircleAvatar(
+      backgroundColor: backgroundColor,
+      radius: radius + outRadius,
+
+      child: CircleAvatar(
+        radius: radius,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: image
+        ),
+      ),
+    );
   }
 }
